e3c934276bac30fd531e3b6183d7edff2154bb7e
YanehCheck <101875519+YanehCheck@users.noreply.github.com>
2024-07-25 02:09:29 +0200

    feat: User settings provider - ISettingsProvider

---
+++ YanehCheck.EpicGamesUtils.WpfUiApp/Services/Interfaces/ISettingsProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace YanehCheck.EpicGamesUtils.WpfUiApp.Services.Interfaces;
+
+public interface ISettingsProvider {
+    public string AccessToken { get; set; }
+    public DateTime AccessTokenExpiry { get; set; }
+    public void Save();
+    public void Reset();
+}
---
+++ YanehCheck.EpicGamesUtils.WpfUiApp/Services/SettingsProvider.cs
@@ -0,0 +1,19 @@
+﻿using YanehCheck.EpicGamesUtils.WpfUiApp.Properties;
+using YanehCheck.EpicGamesUtils.WpfUiApp.Services.Interfaces;
+
+namespace YanehCheck.EpicGamesUtils.WpfUiApp.Services;
+
+public class SettingsProvider : ISettingsProvider {
+    public string AccessToken {
+        get => Settings.Default.AccessToken;
+        set => Settings.Default.AccessToken = value;
+    }
+
+    public DateTime AccessTokenExpiry {
+        get => Settings.Default.AccessTokenExpiry;
+        set => Settings.Default.AccessTokenExpiry = value;
+    }
+
+    public void Save() => Settings.Default.Save();
+    public void Reset() => Settings.Default.Reset();
+}

16c6516b626d786edad5b123a02f9ebb1bb3ec12
YanehCheck <101875519+YanehCheck@users.noreply.github.com>
2024-07-26 21:32:07 +0200

    dev: Progress on home page

--- YanehCheck.EpicGamesUtils.WpfUiApp/Services/SettingsProvider.cs
+++ YanehCheck.EpicGamesUtils.WpfUiApp/Services/SettingsProvider.cs
@@ -1,9 +1,17 @@
-﻿using YanehCheck.EpicGamesUtils.WpfUiApp.Properties;
+﻿using Microsoft.Extensions.Options;
+using YanehCheck.EpicGamesUtils.WpfUiApp.Properties;
 using YanehCheck.EpicGamesUtils.WpfUiApp.Services.Interfaces;
+using YanehCheck.EpicGamesUtils.WpfUiApp.Services.Options;
 
 namespace YanehCheck.EpicGamesUtils.WpfUiApp.Services;
 
 public class SettingsProvider : ISettingsProvider {
+    public SettingsProvider(IOptions<UserOptions> userOptions) {
+        if (userOptions.Value.ResetUserSettings) {
+            Reset();
+        }
+    }
+
     public string AccessToken {
         get => Settings.Default.AccessToken;
         set => Settings.Default.AccessToken = value;
--- YanehCheck.EpicGamesUtils.WpfUiApp/ViewModels/Pages/HomeViewModel.cs
+++ YanehCheck.EpicGamesUtils.WpfUiApp/ViewModels/Pages/HomeViewModel.cs
@@ -1,14 +1,30 @@
-﻿using YanehCheck.EpicGamesUtils.WpfUiApp.Services.Interfaces;
+﻿using Wpf.Ui.Controls;
+using YanehCheck.EpicGamesUtils.WpfUiApp.Services.Interfaces;
 
 namespace YanehCheck.EpicGamesUtils.WpfUiApp.ViewModels.Pages;
 
-public partial class HomeViewModel(IBrowserService browserService) : ObservableObject, IViewModel {
+public partial class HomeViewModel(IBrowserService browserService, ISettingsProvider settingsProvider) : ObservableObject, IViewModel, INavigationAware {
+    private readonly bool _isInitialized = false;
+
     [ObservableProperty] 
     private string _authorizationCode;
+    public DateTime AccessTokenExpiry => settingsProvider.AccessTokenExpiry;
+
+    public void OnNavigatedTo() {
+        if (!_isInitialized) {
+
+        }
+    }
+
+    private void InitializeViewModel() {
+    }
+
+    public void OnNavigatedFrom() { }
 
     [RelayCommand]
     public async Task OnButtonConfirmClick() {
-
+        settingsProvider.AccessToken = AuthorizationCode;
+        settingsProvider.Save();
     }
 
     [RelayCommand]

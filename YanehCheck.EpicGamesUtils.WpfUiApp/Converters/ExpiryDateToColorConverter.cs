fd701b0e33c4d29d073aedca2ed565fd9bf9aa56
YanehCheck <101875519+YanehCheck@users.noreply.github.com>
2024-07-27 02:06:20 +0200

    dev: Added EnumToStringConverter and renamed others

--- YanehCheck.EpicGamesUtils.WpfUiApp/Converters/ExpiryDateToColorConverter.cs
+++ YanehCheck.EpicGamesUtils.WpfUiApp/Converters/ExpiryDateToColorConverter.cs
@@ -1,19 +1,18 @@
 ﻿using System.Globalization;
 using System.Windows.Data;
+using System.Windows.Media;
 
 namespace YanehCheck.EpicGamesUtils.WpfUiApp.Converters;
 
-public class DateToStringConverter : IValueConverter {
+public class ExpiryDateToColorConverter : IValueConverter {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
         if(value is DateTime date) {
-            return date == DateTime.MinValue ? 
-                "Not logged in." :
-                date > DateTime.Now ? 
-                $"Session expires at: {date:yyyy-M-d HH:mm:ss}" :
-                "Expired.";
+            return date < DateTime.Now ? 
+                Application.Current.Resources["SystemFillColorCriticalBrush"] ?? Brushes.Red :
+                Application.Current.Resources["SystemFillColorSuccessBrush"] ?? Brushes.GreenYellow;
         }
 
-        return "Not logged in.";
+        return Brushes.Black;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {

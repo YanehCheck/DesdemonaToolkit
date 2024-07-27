d9a25a3e9dd1ba9d9c43e408f4f242bce7e45912
YanehCheck <101875519+YanehCheck@users.noreply.github.com>
2024-07-27 03:27:42 +0200

    dev: Added options to item providers

--- YanehCheck.EpicGamesUtils.WpfUiApp/Services/FortniteItems/FortniteGgItemProvider.cs
+++ YanehCheck.EpicGamesUtils.WpfUiApp/Services/FortniteItems/FortniteGgItemProvider.cs
@@ -1,15 +1,17 @@
-﻿using YanehCheck.EpicGamesUtils.BL.Models;
+﻿using Microsoft.Extensions.Options;
+using YanehCheck.EpicGamesUtils.BL.Models;
 using YanehCheck.EpicGamesUtils.FortniteGGScraper;
 using YanehCheck.EpicGamesUtils.WpfUiApp.Services.FortniteItems.Interfaces;
 using YanehCheck.EpicGamesUtils.WpfUiApp.Services.FortniteItems.Mappers;
+using YanehCheck.EpicGamesUtils.WpfUiApp.Services.Options;
 
 namespace YanehCheck.EpicGamesUtils.WpfUiApp.Services.FortniteItems;
 
-public class FortniteGgItemProvider(IFortniteGgScrapper fortniteGgScrapper, IFortniteGgItemMapper mapper) : IFortniteGgItemProvider
+public class FortniteGgItemProvider(IFortniteGgScrapper fortniteGgScrapper, IFortniteGgItemMapper mapper, IOptions<ItemFetchOptions> itemOptions) : IFortniteGgItemProvider
 {
     public async Task<IEnumerable<ItemFullModel>?> GetItemsAsync()
     {
-        var items = await fortniteGgScrapper.ScrapIdRangeParallelAsync(0, 14000);
+        var items = await fortniteGgScrapper.ScrapIdRangeParallelAsync(0, itemOptions.Value.FortniteGgIdRange);
         return items is null or { IsEmpty: true } ?
             null :
             items.Select(mapper.MapToModel);
--- YanehCheck.EpicGamesUtils.WpfUiApp/Services/FortniteItems/UriItemProvider.cs
+++ YanehCheck.EpicGamesUtils.WpfUiApp/Services/FortniteItems/UriItemProvider.cs
@@ -1,15 +1,17 @@
 ﻿using System.Text.Json;
+using Microsoft.Extensions.Options;
 using RestSharp;
 using YanehCheck.EpicGamesUtils.BL.Models;
 using YanehCheck.EpicGamesUtils.WpfUiApp.Services.FortniteItems.Interfaces;
+using YanehCheck.EpicGamesUtils.WpfUiApp.Services.Options;
 
 namespace YanehCheck.EpicGamesUtils.WpfUiApp.Services.FortniteItems;
 
-public class UriItemProvider(IRestClient restClient) : IUriItemProvider
+public class UriItemProvider(IRestClient restClient, IOptions<ItemFetchOptions> options) : IUriItemProvider
 {
     public async Task<IEnumerable<ItemFullModel>?> GetItemsAsync()
     {
-        var request = new RestRequest("123");
+        var request = new RestRequest(options.Value.StableSourceUri);
         var response = await restClient.GetAsync(request);
         return response.IsSuccessful ?
             (List<ItemFullModel>)JsonSerializer.Deserialize(response.Content!, typeof(List<ItemFullModel>))! :

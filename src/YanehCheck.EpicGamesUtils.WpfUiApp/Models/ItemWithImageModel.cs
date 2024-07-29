d05bded85e9e539488e21f0e68080851625d05da
YanehCheck <101875519+YanehCheck@users.noreply.github.com>
2024-07-29 03:46:11 +0200

    feat: Basic prototype of inventory fetching

---
+++ src/YanehCheck.EpicGamesUtils.WpfUiApp/Models/ItemWithImageModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Media.Imaging;
+using YanehCheck.EpicGamesUtils.BL.Models;
+
+namespace YanehCheck.EpicGamesUtils.WpfUiApp.Models;
+
+public class ItemWithImageModel : ItemModel {
+    public BitmapFrame? BitmapFrame { get; set; }
+
+    public ItemWithImageModel() { }
+
+    public ItemWithImageModel(ItemModel item) {
+        Id = item.Id;
+        FortniteId = item.FortniteId;
+        FortniteGgId = item.FortniteGgId;
+        Name = item.Name;
+        Description = item.Description;
+        PriceVbucks = item.PriceVbucks;
+        PriceUsd = item.PriceUsd;
+        Season = item.Season;
+        Source = item.Source;
+        SourceDescription = item.SourceDescription;
+        Rarity = item.Rarity;
+        Type = item.Type;
+        Set = item.Set;
+        Release = item.Release;
+        LastSeen = item.LastSeen;
+        Styles = item.Styles ?? [];
+        Tags = item.Tags ?? [];
+    }
+}
--- src/YanehCheck.EpicGamesUtils.WpfUiApp/ViewModels/Pages/ItemsViewModel.cs
+++ src/YanehCheck.EpicGamesUtils.WpfUiApp/ViewModels/Pages/ItemsViewModel.cs
@@ -1,44 +1,67 @@
-﻿using System.Windows.Media;
+﻿using Wpf.Ui;
 using Wpf.Ui.Controls;
+using YanehCheck.EpicGamesUtils.BL.Facades.Interfaces;
+using YanehCheck.EpicGamesUtils.BL.Models;
 using YanehCheck.EpicGamesUtils.WpfUiApp.Models;
+using YanehCheck.EpicGamesUtils.WpfUiApp.Services.EpicGames.Interfaces;
+using YanehCheck.EpicGamesUtils.WpfUiApp.Services.FortniteItems.Interfaces;
+using YanehCheck.EpicGamesUtils.WpfUiApp.Services.Interfaces;
 
 namespace YanehCheck.EpicGamesUtils.WpfUiApp.ViewModels.Pages;
 
-public partial class ItemsViewModel : ObservableObject, IViewModel, INavigationAware {
-    private bool _isInitialized = false;
+public partial class ItemsViewModel(
+    IEpicGamesService epicGamesService,
+    IItemFacade itemFacade,
+    ISessionService sessionService,
+    ISnackbarService snackbarService,
+    IFortniteGgImageDownloader imageDownloader) : ObservableObject, IViewModel, INavigationAware {
+    private string _initializedForAccountId = "";
 
     [ObservableProperty]
-    private IEnumerable<DataColor> _colors;
+    private IEnumerable<ItemWithImageModel> _items = [];
 
     public void OnNavigatedTo() {
-        if(!_isInitialized) {
-            InitializeViewModel();
+        if(!sessionService.IsAuthenticated) {
+            // HANDLE
+            //return;
+        }
+
+        if(!sessionService.IsItemDataFetched) {
+            // HANDLE
+            //return;
+        }
+
+        if(_initializedForAccountId != sessionService.AccountId) {
+            InitializeViewModel().ConfigureAwait(false); // Await or run task
         }
     }
 
     public void OnNavigatedFrom() { }
 
-    private void InitializeViewModel() {
-        var random = new Random();
-        var colorCollection = new List<DataColor>();
+    private async Task InitializeViewModel() {
+        await LoadItems();
+        _initializedForAccountId = sessionService.AccountId!;
+    }
 
-        for(int i = 0; i < 8192; i++) {
-            colorCollection.Add(
-                new DataColor {
-                    Color = new SolidColorBrush(
-                        Color.FromArgb(
-                            (byte) 200,
-                            (byte) random.Next(0, 250),
-                            (byte) random.Next(0, 250),
-                            (byte) random.Next(0, 250)
-                        )
-                    )
-                }
-            );
-        }
+    private async Task LoadItems() {
+        var items = await FetchItems();
+        await Parallel.ForEachAsync(items, async (item, _) => {
+            if(item.FortniteGgId == null) {
+                // 
+                return;
+            }
 
-        Colors = colorCollection;
+            item.BitmapFrame = await imageDownloader.GetImageAsync(item.FortniteGgId);
+            Items = Items.Append(item);
+        });
+    }
 
-        _isInitialized = true;
+    private async Task<IEnumerable<ItemWithImageModel>> FetchItems() {
+        // Handle error
+        var ownedItems = await epicGamesService.GetItems(sessionService.AccountId!, sessionService.AccessToken!);
+        var ownedItemModels = ownedItems.Items!.Select(i => new ItemModel() {
+            FortniteId = i.FortniteId.Split(':').Last()
+        });
+        return (await itemFacade.GetByFortniteIdAsync(ownedItemModels)).Select(i => new ItemWithImageModel(i));
     }
 }

7c1f3c1c14310d48a010f314024a226195694926
YanehCheck <101875519+YanehCheck@users.noreply.github.com>
2024-08-01 22:52:57 +0200

    dev: File save dialog service

---
+++ src/YanehCheck.EpicGamesUtils.WpfUiApp/Services/FileSaveDialogService.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using Microsoft.Win32;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+using YanehCheck.EpicGamesUtils.WpfUiApp.Services.Interfaces;
+
+namespace YanehCheck.EpicGamesUtils.WpfUiApp.Services;
+
+public class FileSaveDialogService : IFileSaveDialogService {
+    public void SaveTextFile(string content, string fileName, string defaultExt = ".txt", string filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*") {
+        var dialog = new SaveFileDialog {
+            FileName = fileName,
+            DefaultExt = defaultExt,
+            Filter = filter
+        };
+
+        if(dialog.ShowDialog() == true) {
+            string filePath = dialog.FileName;
+            File.WriteAllText(filePath, content);
+        }
+    }
+
+    public void SaveImageFile(Image<Rgba32> image, string fileName, string defaultExt = ".png", string filter = "Image files (*.png)|*.png|All files (*.*)|*.*") {
+        var dialog = new SaveFileDialog {
+            FileName = fileName,
+            DefaultExt = defaultExt,
+            Filter = filter
+        };
+
+        if(dialog.ShowDialog() == true) {
+            string filePath = dialog.FileName;
+            using(var stream = new FileStream(filePath, FileMode.Create)) {
+                image.Save(stream, new PngEncoder());
+            }
+        }
+    }
+}
---
+++ src/YanehCheck.EpicGamesUtils.WpfUiApp/Services/Interfaces/IFileSaveDialogService.cs
@@ -0,0 +1,9 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace YanehCheck.EpicGamesUtils.WpfUiApp.Services.Interfaces;
+
+public interface IFileSaveDialogService {
+    void SaveTextFile(string content, string fileName, string defaultExt = ".txt", string filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*");
+    void SaveImageFile(Image<Rgba32> image, string fileName, string defaultExt = ".png", string filter = "Image files (*.png)|*.png|All files (*.*)|*.*");
+}

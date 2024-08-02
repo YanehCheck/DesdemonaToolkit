d4924413e8179ab6a7d0a7d84f58d30f7ec3859e
YanehCheck <101875519+YanehCheck@users.noreply.github.com>
2024-08-02 16:51:59 +0200

    feat: Turn off Export button if there are no Items in selection

---
+++ src/YanehCheck.EpicGamesUtils.WpfUiApp/Converters/General/EnumerableAnyToBoolConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace YanehCheck.EpicGamesUtils.WpfUiApp.Converters.General;
+
+public class EnumerableAnyToBoolConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null) {
+            return false;
+        }
+
+        if (value is IEnumerable list)
+        {
+            if (list.Cast<object?>().Any()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}

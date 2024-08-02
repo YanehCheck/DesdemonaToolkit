2c4e56b45f8d426cbc8e720f5c82615b39c238d6
YanehCheck <101875519+YanehCheck@users.noreply.github.com>
2024-08-02 02:48:52 +0200

    dev: Added spacing between items in image export

--- src/YanehCheck.EpicGamesUtils.WpfUiApp/Services/FortniteItems/FortniteInventoryImageProcessor.cs
+++ src/YanehCheck.EpicGamesUtils.WpfUiApp/Services/FortniteItems/FortniteInventoryImageProcessor.cs
@@ -12,13 +12,18 @@ public interface IFortniteInventoryImageProcessor {
 }
 public class FortniteInventoryImageProcessor : IFortniteInventoryImageProcessor {
     // TODO: Un-hardcode this
-    private readonly int itemsPerRow = 10;
     private readonly int itemHeight = 260;
     private readonly int itemWidth = 210;
-    private readonly Color backgroundColor = Color.Black;
+    private readonly Color backgroundColor = Color.Gray;
+    private readonly int spacing = 5;
+
+    private int itemsPerRow = 10;
 
     public Image Create(List<ItemPresentationModel> items) {
-        
+        if (items.Count < 10) {
+            itemsPerRow = items.Count;
+        }
+
         var width = itemsPerRow * itemWidth;
         var height = (int) Math.Ceiling((double) items.Count / itemsPerRow) * itemHeight;
         var image = new Image<Bgra32>(width, height);
@@ -28,8 +33,8 @@ public class FortniteInventoryImageProcessor : IFortniteInventoryImageProcessor 
         for (int i = 0; i < items.Count; i++) {
             var row = i / itemsPerRow;
             var col = i % itemsPerRow;
-            var x = col * itemWidth;
-            var y = row * itemHeight;
+            var x = col * (itemWidth + spacing);
+            var y = row * (itemHeight + spacing);
 
             // TODO: Handle this
             using var itemImage = items[i].BitmapFrame!.ToImageSharpImage();

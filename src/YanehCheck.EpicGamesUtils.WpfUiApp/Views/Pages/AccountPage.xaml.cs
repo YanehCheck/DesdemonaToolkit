2ee0cdeed8a1c2d768a42e178778e8a8273b9b5a
YanehCheck <101875519+YanehCheck@users.noreply.github.com>
2024-08-05 21:52:18 +0200

    dev: Added VM and V

---
+++ src/YanehCheck.EpicGamesUtils.WpfUiApp/Views/Pages/AccountPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Wpf.Ui.Controls;
+using YanehCheck.EpicGamesUtils.WpfUiApp.ViewModels.Pages;
+
+namespace YanehCheck.EpicGamesUtils.WpfUiApp.Views.Pages;
+
+public partial class AccountPage : INavigableView<AccountViewModel> {
+    public AccountViewModel ViewModel { get; }
+
+    public AccountPage(AccountViewModel viewModel) {
+        ViewModel = viewModel;
+        DataContext = this;
+
+        InitializeComponent();
+    }
+}

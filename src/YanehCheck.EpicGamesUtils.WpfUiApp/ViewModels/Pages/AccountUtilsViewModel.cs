d5259bf14588480b14906b67564a042f979e2e48
YanehCheck <101875519+YanehCheck@users.noreply.github.com>
2024-10-01 20:58:08 +0200

    feat: Fast redeem by monitoring clipboard

--- src/YanehCheck.EpicGamesUtils.WpfUiApp/ViewModels/Pages/AccountUtilsViewModel.cs
+++ src/YanehCheck.EpicGamesUtils.WpfUiApp/ViewModels/Pages/AccountUtilsViewModel.cs
@@ -1,18 +1,33 @@
 ﻿using System.ComponentModel;
 using Wpf.Ui;
 using Wpf.Ui.Controls;
+using YanehCheck.EpicGamesUtils.EgsApi.Api.Exceptions;
 using YanehCheck.EpicGamesUtils.EgsApi.Service;
 using YanehCheck.EpicGamesUtils.WpfUiApp.Services.Persistence.Interfaces;
+using YanehCheck.EpicGamesUtils.WpfUiApp.Services.UI;
+using YanehCheck.EpicGamesUtils.WpfUiApp.Services.UI.Interfaces;
 
 namespace YanehCheck.EpicGamesUtils.WpfUiApp.ViewModels.Pages;
 
 public partial class AccountUtilsViewModel(
     IEpicGamesService epicGamesService,
     ISnackbarService snackbarService,
-    ISessionService sessionService) : ObservableObject, IViewModel, INotifyPropertyChanged {
+    ISessionService sessionService,
+    IClipboardService clipboardService) : ObservableObject, IViewModel, INotifyPropertyChanged, INavigationAware {
 
-    [ObservableProperty]
-    private string _code = string.Empty;
+    [ObservableProperty] private string _code = string.Empty;
+    [ObservableProperty] private bool _monitorClipboardRedeemCode;
+
+    private bool _initialized;
+
+    public void OnNavigatedTo() {
+        if(!_initialized) {
+            clipboardService.OnClipboardChanged += async (s, a) => await OnClipboardChanged(s, a);
+            _initialized = true;
+        }
+    }
+
+    public void OnNavigatedFrom() { }
 
     private bool AuthCheck() {
         if(!sessionService.IsAuthenticated) {
@@ -24,7 +39,69 @@ public partial class AccountUtilsViewModel(
                 TimeSpan.FromSeconds(5));
             return false;
         }
+
         return true;
+    }
+
+    private async Task OnClipboardChanged(object? sender, ClipboardService.ClipboardChangedEventArgs args) {
+        if(args.Type != ClipboardService.ContentType.Text) {
+            return;
+        }
+
+        var content = (args.Content as string)!;
+        var realCode = string.Concat(content.Split(' ', '-'));
+        if(realCode is not { Length: > 5 and < 30 }) {
+            return;
+        }
+        if (!realCode.All(char.IsAsciiLetterOrDigit)) {
+            return;
+        }
+
+        try {
+            await epicGamesService.RedeemCodeAccount(sessionService.AccountId!, sessionService.AccessToken!, realCode);
+        }
+        catch(EpicGamesApiException ex) {
+            switch (ex.ErrorCode) {
+                // Let's ignore not found.
+                case "errors.com.epicgames.coderedemption.code_not_found":
+                    return;
+                // These should probably be alerted though
+                // case "errors.com.epicgames.coderedemption.code_expired":
+                // case "errors.com.epicgames.common.throttled":
+                // case "errors.com.epicgames.coderedemption.code_used":
+                default:
+                    await ShowSnackbarOnUIThread(
+                        "Failure",
+                        ex.Message,
+                        ControlAppearance.Danger,
+                        TimeSpan.FromSeconds(10));
+                    return;
+            }
+        }
+        await ShowSnackbarOnUIThread(
+            "Success",
+            "Code redeemed.",
+            ControlAppearance.Success,
+            TimeSpan.FromSeconds(15));
+    }
+
+    private Task ShowSnackbarOnUIThread(string title, string message, ControlAppearance appearance, TimeSpan duration) {
+        return Application.Current.Dispatcher.InvokeAsync(() =>
+        {
+            snackbarService.Show(title, message, appearance, null, duration);
+        }).Task;
+    }
+
+
+
+    [RelayCommand]
+    private void ToggleRedeemCodeMonitorClipboard() {
+        if(MonitorClipboardRedeemCode) {
+            clipboardService.Start();
+        }
+        else {
+            clipboardService.Stop();
+        }
     }
 
     [RelayCommand]
@@ -35,9 +112,9 @@ public partial class AccountUtilsViewModel(
 
         var realCode = string.Concat(Code.Split(' ', '-'));
 
-        /* TODO: Some research on format
-        if(realCode is not { Length: 16 or 20 }) { 
-        }*/
+        // TODO: Format check?
+        // Maybe it's better to keep this more permissive?s
+
         try {
             await epicGamesService.RedeemCodeAccount(sessionService.AccountId!, sessionService.AccessToken!, realCode);
         }
